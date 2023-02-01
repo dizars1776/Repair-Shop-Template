a85bd7be08f223441da8a041b94ad0cfbb58fac0
dizars1776 <dizars1776@yahoo.gr>
2023-02-01 13:45:07 +0200

    feat: add skeleton

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex w-screen justify-center">
+      <h1 className="text-3xl font-bold underline">Camera Service</h1>
+    </div>
+  )
+}
+export default App
