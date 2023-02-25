978aee8546530a8a8f1341c41da7ee53b5824bb5
Nikolaos Georgiou <dizars1776@yahoo.gr>
2023-02-26 00:19:14 +0200

    fix: update mobile menu

---
+++ src/components/menu/useMenuTrigger.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+
+function useMenuTrigger(initState = false) {
+  const [menuBtnVisibility, setMenuBtnVisibility] = useState(initState)
+  const [isMenuOpen, setIsMenuOpen] = useState(initState)
+
+  const menuTrigger = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+  // give it time to navigate to the section and then hide
+  const hide = () => {
+    setTimeout(() => {
+      setIsMenuOpen(false)
+    }, 500)
+  }
+  const show = () => setIsMenuOpen(true)
+
+  // make mobile menu button visible when scrolling below 250
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 250) {
+        setMenuBtnVisibility(true)
+      } else {
+        setMenuBtnVisibility(false)
+        setIsMenuOpen(false)
+      }
+    })
+  }, [])
+  // trigger toggle for the mobile menu
+  useEffect(() => {}, [])
+
+  return {
+    btnVisibility: menuBtnVisibility,
+    trigger: menuTrigger,
+    isOpen: isMenuOpen,
+    hide: hide,
+    show: show,
+  }
+}
+
+export default useMenuTrigger
