cf47e3f9fb31f48b9c155e7ebd2d0812f5775e11
Nikolaos Georgiou <dizars1776@yahoo.gr>
2023-04-12 13:28:33 +0300

    refactor(dropdown)

--- src/components/menu/Dropdown.jsx
+++ src/components/menu/Dropdown.jsx
@@ -2,7 +2,7 @@ import LanguageSwitch from '../switches/languageSwitch/LanguageSwitch'
 import ThemeSwitch from '../switches/themeSwitch/ThemeSwitch'
 
 const Dropdown = ({ hasSettings, children }) => {
-  console.log(hasSettings)
+
   return (
     <div className="absolute right-0 z-50 min-w-max translate-y-3 translate-x-0 overflow-hidden rounded-2xl border border-t-4 border-sky-800 bg-white py-1 md:w-64">
       <div className="flex flex-col gap-y-4 p-4">
